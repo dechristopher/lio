88b76208e05c89862e738e0eb0a5b31713f911ca
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-03-02 20:13:27 -0500

    fix: typo in deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,7 +11,7 @@ docker image prune -f --all
 cd ../src && docker build -t lioctad:latest -f Dockerfile .
 
 # deploy and scale service down
-cd ../deploy && docker stack deploy -c lioctad-stackyml lio-prod
+cd ../deploy && docker stack deploy -c lioctad-stack.yml lio-prod
 docker service scale lio-prod_lioctad=1
 
 # watch the magic
