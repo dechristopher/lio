5677bb50f078b7ba41211359dbb5f00917c917e0
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-03-05 09:53:07 -0500

    feat: added octadground.js build and package to deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,12 +3,21 @@
 # WARNING. This is a crude and destructive deploy script.
 # It makes no guarantees of uptime or stability.
 
+# build new octadground dist
+cd ~/octadground && git pull && yarn run dist
+
+# copy dist into lioctad res directory
+cp ~/octadground/dist/octadground.js ~/lioctad/src/static/res
+
 # remove and prune old stack
 docker stack rm lio-prod
 docker image prune -f
 
 # rebuild lioctad container
 cd ../src && docker build -t lioctad:latest -f Dockerfile .
+
+# remove octadground dist so we don't have local changes
+rm -rf ~/lioctad/src/static/res/octadground.js
 
 # deploy and scale service down
 cd ../deploy && docker stack deploy -c lioctad-stack.yml lio-prod
