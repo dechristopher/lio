ed0c29dfb703807c80ee9ecbe5e9b2613391d8ae
Mike Upton <me@uptonm.dev>
2021-03-06 15:48:39 -0500

    feat: initial layout system boilerplate

--- ui/src/app/views/Play/Play.tsx
+++ ui/src/app/views/Play/Play.tsx
@@ -2,7 +2,7 @@ import React, {FC} from "react";
 
 export const PlayView: FC = () => {
 	return (
-		<div className="mt-16 w-screen" style={{height: "calc(100vh - 4rem)"}}>
+		<div className="mt-16 w-screen flex flex-col items-stretch overflow-x-hidden overflow-y-auto" style={{height: "calc(100vh - 4rem)"}}>
 			<div className="md:flex md:items-center md:justify-between pt-8 pb-36 bg-green-600">
 				<div className="px-6 flex-1 min-w-0">
 					<h2 className="text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate">
@@ -10,24 +10,46 @@ export const PlayView: FC = () => {
 					</h2>
 				</div>
 			</div>
-			<div className="flex-1 relative z-0 flex overflow-hidden h-full w-full -mt-28">
-				<main className="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabIndex={0}>
+			<div className="flex-1 relative z-0 flex -mt-28">
+				<main className="flex-1 relative z-0 focus:outline-none space-y-8" tabIndex={0}>
+					<div className="absolute inset-0 py-6 px-2 sm:px-4 lg:px-4">
 					{/* <!-- Start main area--> */}
-					<div className="absolute inset-0 py-6 px-2 sm:px-4 lg:px-4">
-							<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 sm:gap-6 lg:gap-8">
-								<div className="border-2 border-black border-dashed rounded-lg h-48" />
-								<div className="border-2 border-black border-dashed rounded-lg h-48" />
-								<div className="border-2 border-black border-dashed rounded-lg h-48" />
-								<div className="border-2 border-black border-dashed rounded-lg h-48" />
+
+						<div className="border-2 border-black border-dashed rounded-lg h-16 flex md:hidden">
+							Online Player Stats
 						</div>
-					</div>
+
+						<div className="border-2 border-black border-dashed rounded-lg h-96 mt-8 md:mt-0">
+								Variant Selector
+						</div>
+
+						<div className="border-2 border-black border-dashed rounded-lg h-96 mt-8">
+							Custom Games
+						</div>
+
+						<div className="border-2 border-black border-dashed rounded-lg h-96 flex md:hidden mt-8 md:mt-0">
+							Top Players
+						</div>
+
+						<div className="border-2 border-black border-dashed rounded-lg h-16 flex md:hidden mt-8 md:mt-0">
+							Footer
+						</div>
 					{/* <!-- End main area --> */}
+					</div>
 				</main>
-				<aside className="hidden relative lg:flex lg:flex-col flex-shrink-0 w-96">
+				<aside className="hidden relative md:flex md:flex-col flex-shrink-0 w-4/12">
 					{/* <!-- Start secondary column (hidden on smaller screens) --> */}
-					<div className="absolute inset-0 py-6 px-2 sm:px-4 lg:px-4">
-						<div className="h-full">
-							<div className="border-2 border-black border-dashed rounded-lg h-96" />
+					<div className="absolute inset-0 py-6 px-2 sm:px-4 lg:px-4 space-y-8">
+						<div className="border-2 border-black border-dashed rounded-lg h-96">
+							Top Players
+						</div>
+
+						<div className="border-2 border-black border-dashed rounded-lg h-16">
+							Online Player Stats
+						</div>
+
+						<div className="border-2 border-black border-dashed rounded-lg h-16">
+							Footer
 						</div>
 					</div>
 					{/* <!-- End secondary column --> */}
