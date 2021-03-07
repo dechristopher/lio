c32240156a2b26773a4869ffb9cc449b31860fa4
Mike Upton <me@uptonm.dev>
2021-03-07 17:51:26 -0500

    feat: adds tos to footer component

--- ui/src/app/components/Footer/Footer.tsx
+++ ui/src/app/components/Footer/Footer.tsx
@@ -7,6 +7,7 @@ export const Footer: FC = () => {
 			<Link className="whitespace-no-wrap px-2" to="/about">About</Link>
 			<Link className="whitespace-no-wrap px-2" to="/faq">FAQ</Link>
 			<Link className="whitespace-no-wrap px-2" to="/contact">Contact</Link>
+			<Link className="whitespace-no-wrap px-2" to="/tos">Terms of Service</Link>
 			<Link className="whitespace-no-wrap px-2" to="/privacy">Privacy</Link>
 			<a className="whitespace-no-wrap px-2" href="https://github.com/dechristopher/lio">Source Code</a>
 			<a className="whitespace-no-wrap px-2" href="https://github.com/dechristopher/lio/issues">Report an Issue</a>
