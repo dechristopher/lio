dbb55e193ba88c9c013f2851bc471dc5d1b20653
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-03-09 12:44:27 -0500

    fix: proto_crowd doc

--- src/www/ws/proto/proto_crowd.go
+++ src/www/ws/proto/proto_crowd.go
@@ -2,7 +2,7 @@ package proto
 
 import "github.com/dechristopher/lioctad/www/ws/common"
 
-// Marshal fully JSON marshals the MovePayload and
+// Marshal fully JSON marshals the CrowdPayload and
 // Wraps it in a Message struct
 func (c *CrowdPayload) Marshal() []byte {
 	message := Message{
@@ -13,6 +13,8 @@ func (c *CrowdPayload) Marshal() []byte {
 	return message.Please()
 }
 
+// Broadcast will send a Crowd message to all sockets connected
+// to the channel within the meta given
 func (c CrowdPayload) Broadcast(meta common.SocketMeta) {
 	common.Broadcast(c.Marshal(), meta)
 }
