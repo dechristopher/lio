e80383f5836ea1b0cb7ba401c07d5cec68f34e13
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-03-16 19:03:14 -0400

    feat: created default rating pool groups

---
+++ src/pools/pools.go
@@ -0,0 +1,26 @@
+package pools
+
+import "github.com/dechristopher/lioctad/variant"
+
+// RatingPools is a map of all active competitive octad variants
+// on the site grouped by variant group as the individual pools
+var RatingPools = map[variant.Group][]variant.Variant{
+	variant.BulletGroup: {
+		variant.QuarterZeroBullet,
+		variant.QuarterOneBullet,
+	},
+	variant.BlitzGroup: {
+		variant.HalfZeroBlitz,
+		variant.HalfOneBlitz,
+	},
+	variant.RapidGroup: {
+		variant.OneZeroRapid,
+		variant.OneTwoRapid,
+	},
+	variant.HyperGroup: {
+		variant.FiveZeroHyper,
+	},
+	variant.UltiGroup: {
+		variant.ZeroFiveUlti,
+	},
+}
