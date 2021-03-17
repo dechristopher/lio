4a9c7ef3161b16fcdcbc69dce0046eae0959535d
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-03-17 18:47:50 -0400

    feat: basic engine monitoring over engine bus channel

---
+++ src/engine/monitor.go
@@ -0,0 +1,30 @@
+package engine
+
+import (
+	"github.com/dechristopher/lioctad/bus"
+	"github.com/dechristopher/lioctad/str"
+	"github.com/dechristopher/lioctad/util"
+)
+
+// init the engine monitoring
+func init() {
+	go monitorSub()
+}
+
+// monitorSub creates the engine monitoring subscription
+func monitorSub() {
+	err := Channel.Subscribe(monitorEngine)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// monitor engine watches the bus channel and catalogues
+// all engine search output events into a datastore
+func monitorEngine(e bus.Event) {
+	if len(e.Data) == 2 {
+		util.Debug(str.CEng, str.DEngStart, e.Data...)
+		return
+	}
+	util.Debug(str.CEng, str.DEngSearch, e.Data...)
+}
