2d5f7be4f61b8a4e3f95cb0ea7429eddaac20777
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-03-17 17:07:48 -0400

    feat: implemented internal event bus for event-driven actions

---
+++ src/bus/bus.go
@@ -0,0 +1,69 @@
+package bus
+
+import (
+	"time"
+
+	ev "github.com/asaskevich/EventBus"
+)
+
+// Internal event bus implementation
+var bus ev.Bus
+var ready = false
+
+// Up brings up the event bus
+func Up() {
+	bus = ev.New()
+	ready = true
+}
+
+// Event that's published to the bus by a Publisher
+type Event struct {
+	Publisher string
+	Data      []interface{}
+}
+
+// Publisher is a struct representing a
+// named publisher to a channel
+type Publisher struct {
+	Name string
+	Channel
+}
+
+// NewPublisher returns a new Publisher instance
+// configured for the given Channel
+func NewPublisher(name string, channel Channel) *Publisher {
+	return &Publisher{
+		Name:    name,
+		Channel: channel,
+	}
+}
+
+// Publish an event to the Publisher's configured channel
+func (p *Publisher) Publish(data ...interface{}) {
+	for ready == false {
+		time.Sleep(time.Millisecond * 5)
+	}
+	bus.Publish(string(p.Channel), Event{
+		Publisher: p.Name,
+		Data:      data,
+	})
+}
+
+// Channel is a constant for publishing or
+// subscribing to events to the bus
+type Channel string
+
+// Predefined bus channels
+const (
+	SystemChannel Channel = "lio:sys"
+	GameChannel   Channel = "lio:game"
+)
+
+// SubscribeOnce subscribes to the given channel and is removed
+// once the given handler function has been executed.
+func (c Channel) SubscribeOnce(fn func(e Event)) error {
+	for ready == false {
+		time.Sleep(time.Millisecond * 5)
+	}
+	return bus.SubscribeOnce(string(c), fn)
+}
--- src/systems/systems.go
+++ src/systems/systems.go
@@ -1,15 +1,38 @@
 package systems
 
-import "github.com/dechristopher/lioctad/store"
+import (
+	"time"
+
+	"github.com/dechristopher/lioctad/bus"
+	"github.com/dechristopher/lioctad/store"
+	"github.com/dechristopher/lioctad/str"
+	"github.com/dechristopher/lioctad/util"
+)
+
+var pub *bus.Publisher
+
+func Up() {
+	pub = bus.NewPublisher("sys", bus.SystemChannel)
+	time.Sleep(time.Millisecond * 500)
+	pub.Publish("bus online")
+}
 
 // Initializers for subsystem components
 var Initializers = []func(){
+	bus.Up,
 	store.Up,
+	Up,
 }
 
 // Run all of the subsystem initializer functions
 func Run() {
 	for _, i := range Initializers {
 		go i()
+	}
+	err := bus.SystemChannel.SubscribeOnce(func(e bus.Event) {
+		util.Debug(str.CBus, str.DBusOk)
+	})
+	if err != nil {
+		panic(err)
 	}
 }
