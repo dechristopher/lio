47ee552e03df8ac6cadf324830d5872485b32d17
Shawn Toubeau <shawntoubeau@gmail.com>
2021-11-06 16:31:02 -0400

    feat(ui): Sets up desktop grid layout for play screen
    - responsive/mobile WIP

--- ui/src/appv2/views/Play/Play.tsx
+++ ui/src/appv2/views/Play/Play.tsx
@@ -8,7 +8,16 @@ export const PlayView = (): JSX.Element => {
                 padding: 32
             }}
         >
-            Play!
+            <div className="h-full grid grid-rows-2 gap-8">
+                <div>Quick Match</div>
+                <div className="grid grid-cols-2 gap-8">
+                    <div>Join a Game</div>
+                    <div className="grid grid-Rows-2 gap-8">
+                        <div>Top Small Grid</div>
+                        <div>Bottom Small Grid</div>
+                    </div>
+                </div>
+            </div>
         </ContentContainer>
     )
 }
