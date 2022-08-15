781e1b6a334810fdfa1e88996b769f742559bf11
Andrew DeChristopher <me@dchr.host>
2022-08-15 19:42:22 -0400

    feat: BothColors and DoBothColors util functions

--- src/util/tool.go
+++ src/util/tool.go
@@ -3,8 +3,22 @@ package util
 import (
 	"time"
 
+	"github.com/dechristopher/octad"
+
 	"github.com/dechristopher/lioctad/config"
 )
+
+// DoBothColors runs the given function on both colors
+func DoBothColors(f func(color octad.Color)) {
+	f(octad.White)
+	f(octad.Black)
+}
+
+// BothColors returns true if a provided check function returns true
+// when given both white and black
+func BothColors(check func(color octad.Color) bool) bool {
+	return check(octad.White) && check(octad.Black)
+}
 
 // MilliTime returns the current millisecond time
 func MilliTime() int64 {
