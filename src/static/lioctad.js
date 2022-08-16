c555e58688d10be1f8c56cb048e8d172d9a1a4ec
Andrew DeChristopher <me@dchr.host>
2022-08-15 22:24:16 -0400

    feat: requestAnimationFrame clock animations, no intervals
    - Improved timeFormatter logic

--- src/static/lioctad.js
+++ src/static/lioctad.js
@@ -1,8 +1,17 @@
 let ws, ka, backoff = 0, move = 1;
 let pingRunner, lastPingTime, latency = 0, pongCount = 0, pingDelay = 5000;
-let clockTicker, wt, bt;
+let frameId, frameTime, wt, bt;
 
 const logMe = () => console.log(`© 2022 lioctad.org`);
+
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
 
 const isMobile = (function(a, b) {
 	return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4));
@@ -232,7 +241,8 @@ const parseResponse = (raw) => {
 			handleMove(message);
 			break;
 		case "g": // game over
-			clearInterval(clockTicker);
+			// clearInterval(clockTicker);
+			cancelAnimationFrame(frameId);
 			document.getElementById("info").innerHTML = message.d.s;
 			notification.play();
 
@@ -313,7 +323,6 @@ const handleMove = (message) => {
 
 	// perform pre-move if set
 	og.playPremove();
-	console.log("endHandler");
 };
 
 
@@ -325,16 +334,12 @@ const handleMove = (message) => {
  */
 const playSounds = (message, ofenParts) => {
 	if (message.d.gs) {
-		console.log("start");
 		// play confirmation sound on game start
 		confirmation.play();
 	} else {
-		console.log("no start");
-		console.log(ofenParts[0], og.state.ofen, ofenParts[0] !== og.state.ofen);
 		// play move sounds if game is not starting
 		// and only if board ofen is different from current
 		if (message.d.s && ofenParts[0] !== og.state.ofen) {
-			console.log("play move sound");
 			playMoveSound(message.d.s);
 		}
 	}
@@ -346,7 +351,7 @@ const playSounds = (message, ofenParts) => {
  * @param ofenParts - OFEN parts array
  */
 const updateUI = (message, ofenParts) => {
-	clearInterval(clockTicker);
+	// clearInterval(clockTicker);
 
 	wt = message.d.c.w;
 	bt = message.d.c.b;
@@ -393,35 +398,38 @@ const updateUI = (message, ofenParts) => {
 		plyClock.classList.remove('playerWhite');
 	}
 
+	cancelAnimationFrame(frameId);
+
 	// only run this when move is provided, otherwise we flip
 	// the clock on regular game updates, which is not intended
 	if (message.d.m) {
+		// set frame time to compare against
+		frameTime = performance.now();
 		// reset centi-second clock interpolator to decrement correct player
 		if (isPlayerTurn(message, ofenParts)) {
-			clockTicker = setInterval(() => {
-				if (playerTimeRemaining <= 10) {
-					playerTimeRemaining = 0;
-					clearInterval(clockTicker);
-				} else {
-					playerTimeRemaining -= 10;
-				}
-				plyTime.innerHTML = timeFormatter(playerTimeRemaining);
-				plyBar.style.width = barWidth(message.d.c.tc, playerTimeRemaining);
-			}, 100);
+			frameId = requestAnimFrame(clockFrame(playerTimeRemaining, message.d.c.tc, plyTime, plyBar));
 		} else {
-			clockTicker = setInterval(() => {
-				if (opponentTimeRemaining <= 10) {
-					opponentTimeRemaining = 0;
-					clearInterval(clockTicker);
-				} else {
-					opponentTimeRemaining -= 10;
-				}
-				oppTime.innerHTML = timeFormatter(opponentTimeRemaining);
-				oppBar.style.width = barWidth(message.d.c.tc, opponentTimeRemaining);
-			}, 100);
+			frameId = requestAnimFrame(clockFrame(opponentTimeRemaining, message.d.c.tc, oppTime, oppBar));
 		}
 	}
 };
+
+/**
+ * Clock frame generator function
+ * @param timeRemaining - last move message time
+ * @param timeControl - time control, total seconds
+ * @param timeElement - clock time element
+ * @param barElement - clock progress bar element
+ * @returns {(function(): void)|*} frame function
+ */
+const clockFrame = (timeRemaining, timeControl, timeElement, barElement) => () => {
+	const elapsed = (performance.now() - frameTime) * 10;
+	const remaining = ((timeRemaining * 100) - elapsed) / 100; // hacky
+	timeElement.innerHTML = timeFormatter(remaining);
+	barElement.style.width = barWidth(timeControl, remaining);
+
+	frameId = requestAnimFrame(clockFrame(timeRemaining, timeControl, timeElement, barElement));
+}
 
 const padZero = (time, slice) => `0${time}`.slice(slice);
 
@@ -445,7 +453,13 @@ const timeFormatter = (centiseconds) => {
 	}
 	const secondsFmt = padZero(seconds, -2);
 
-	let centiFmt = `${centiseconds % 100}`.slice(0, 1);
+	const centis = (centiseconds % 100);
+	let centiFmt
+	if (centis < 10) {
+		centiFmt = padZero(centis, 0).slice(0, 1);
+	} else {
+		centiFmt = `${centis}`.slice(0, 1);
+	}
 
 	return `${minutesFmt}:${secondsFmt}.${centiFmt}`;
 }
