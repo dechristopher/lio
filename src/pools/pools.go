18b681b6f38013a91a90559a40bda4c93445d978
Andrew DeChristopher <me@dchr.host>
2022-08-24 20:38:10 -0400

    feat: order time controls properly in create game modal

--- src/pools/pools.go
+++ src/pools/pools.go
@@ -7,9 +7,9 @@ var Map map[string]variant.Variant
 func init() {
 	Map = make(map[string]variant.Variant)
 
-	for _, rating_pool := range RatingPools {
-		for _, variant := range rating_pool {
-			Map[variant.HTMLName] = variant
+	for _, ratingPool := range RatingPools {
+		for _, control := range ratingPool {
+			Map[control.HTMLName] = control
 		}
 	}
 }
@@ -17,15 +17,15 @@ func init() {
 // RatingPools is a map of all active competitive octad variants
 // on the site grouped by variant group as the individual pools
 var RatingPools = map[variant.Group][]variant.Variant{
-	variant.BulletGroup: {
+	"0" + variant.BulletGroup: {
 		variant.QuarterZeroBullet,
 		variant.QuarterOneBullet,
 	},
-	variant.BlitzGroup: {
+	"1" + variant.BlitzGroup: {
 		variant.HalfZeroBlitz,
 		variant.HalfOneBlitz,
 	},
-	variant.RapidGroup: {
+	"2" + variant.RapidGroup: {
 		variant.OneZeroRapid,
 		variant.OneTwoRapid,
 	},
