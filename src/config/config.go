a845885a9c4541a9165b962975c39046414b9d9f
Andrew DeChristopher <me@dchr.host>
2026-07-17 11:56:37 -0400

    feat: v0.9.22 release

--- src/config/config.go
+++ src/config/config.go
@@ -19,7 +19,7 @@ const (
 	Base58
 
 	// Version of lio
-	Version = "v0.9.21"
+	Version = "v0.9.22"
 
 	charsetHex    = "abcdef01234567890"
 	charsetBase58 = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789"
