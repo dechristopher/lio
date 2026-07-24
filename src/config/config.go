ce4765f266410b779a182b28a41831dd86d01308
Andrew DeChristopher <me@dchr.host>
2026-07-24 10:01:11 -0400

    feat: v1.5.2 release

--- src/config/config.go
+++ src/config/config.go
@@ -19,7 +19,7 @@ const (
 	Base58
 
 	// Version of lio
-	Version = "v1.5.1"
+	Version = "v1.5.2"
 
 	charsetHex    = "abcdef01234567890"
 	charsetBase58 = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789"
