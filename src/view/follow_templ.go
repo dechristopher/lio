ef4856fcbea8b27cf482c77311156b61e1cac728
Andrew DeChristopher <me@dchr.host>
2026-07-31 12:59:01 -0400

    feat: folded light/dark setting into settings and removed dedicated header button

--- src/view/follow_templ.go
+++ src/view/follow_templ.go
@@ -236,7 +236,7 @@ func followingButton() templ.Component {
 			templ_7745c5c3_Var10 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "<button type=\"button\" id=\"followingButton\" class=\"hdr-icon-btn hdr-badged shrink-0\" aria-label=\"Players you follow\" title=\"Players you follow\" aria-haspopup=\"true\" aria-expanded=\"false\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "<button type=\"button\" id=\"followingButton\" class=\"hdr-icon-btn hdr-badged shrink-0\" aria-label=\"Following\" title=\"Following\" aria-haspopup=\"true\" aria-expanded=\"false\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
